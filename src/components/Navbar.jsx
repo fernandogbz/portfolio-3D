aa4b1fda407ab2972b0244a25cf9ba6a85e19169
fernandogbz <fgjv06@gmail.com>
2024-06-26 00:10:37 -0400

    Space between logo and name fixed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -23,8 +23,9 @@ const Navbar = () => {
           }}
         >
           <img src={fjlogo} alt="logo" className="w-9 h-9 object-contain" />
-          <p className="text-white text-[18px] font-bold cursor pointer flex gap-1">
-            Fernando <span className="sm:block hidden">Jiménez</span>
+          <p className="text-white text-[18px] font-bold cursor pointer flex">
+            Fernando &nbsp;
+            <span className="sm:block hidden">Jiménez</span>
           </p>
         </Link>
         <ul className="list-none hidden sm:flex flex-row gap-10">
