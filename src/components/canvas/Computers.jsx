8dd78cd4c4cf6dfa6b2beb1da607f0153f96f798
fernandogbz <fgjv06@gmail.com>
2024-06-28 02:55:09 -0400

    3D model added and working, ComputersCanvas function is defining a React functional component that renders a 3D scene using the  component from the  library. Inside the , it sets up a camera position, enables shadows, and includes an  component for user interaction

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -12,10 +12,50 @@ const Computers = () => {
   return (
     <mesh>
       <hemisphereLight intensity={0.15} groundColor="black" />
-      <pintlight intensity={1} />
-      <primitive object={computer.scene} />
+      <pointLight intensity={1} />
+      <spotLight
+        position={[-20, 50, 10]}
+        angle={0.12}
+        penumbra={1}
+        intensity={1}
+        castShadow
+        shadow-mapSize={1024}
+      />
+      <primitive
+        object={computer.scene}
+        scale={0.75}
+        position={[0, -3.25, -1.5]}
+        rotation={[-0.01, -0.2, -0.1]}
+      />
     </mesh>
   );
 };
 
-export default Computers;
+// /* The `const ComputersCanvas` function is defining a React functional component that renders a 3D
+// scene using the `Canvas` component from the `@react-three/fiber` library. Inside the `Canvas`, it
+// sets up a camera position, enables shadows, and includes an `OrbitControls` component for user
+// interaction. */
+const ComputersCanvas = () => {
+  return (
+    <Canvas
+      frameLoop="demand"
+      shadows
+      camera={{ position: [20, 3, 5], fov: 25 }}
+      gl={{ preserveDrawingBuffer: true }}
+    >
+      {/* fallback={<CanvasLoader />} Esto va en el suspense*/}
+      <Suspense>
+        <OrbitControls
+          enableZoom={false}
+          maxPolarAngle={Math.PI / 2}
+          minPolarAngle={Math.PI / 2}
+        />
+        <Computers />
+      </Suspense>
+
+      <Preload all />
+    </Canvas>
+  );
+};
+
+export default ComputersCanvas;
