485781ef275ab4a585e992513aab2e307c3ab1c9
fernandogbz <fgjv06@gmail.com>
2024-06-27 21:48:35 -0400

    Name title headText and subHeadText

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -15,6 +15,16 @@ const Hero = () => {
             <div className="w-5 h-5 rounded-full bg-[#915eff]" />
             <div className="w-1 sm:h-80 h-40 violet-gradient" />
           </div>
+          <div>
+            <h1 className={`${styles.heroHeadText} text-white`}>
+              Hola, soy <span className="text-[#915eff]">Fernando</span>
+            </h1>
+            <p className={`${styles.heroSubText} mt-2 text-white`}>
+              {" "}
+              Desarrollo interfaces de usuario{" "}
+              <br className="sm:block hidden" /> y aplicaciones web
+            </p>
+          </div>
         </div>
       </section>
     </>
