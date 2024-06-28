02e1395e2f30c51e5b905ce2811d51855c40b724
fernandogbz <fgjv06@gmail.com>
2024-06-27 22:12:29 -0400

    import canvasLoader

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -4,6 +4,8 @@ import { Canvas } from "@react-three/fiber";
 
 import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 
+import CanvasLoader from "../Loader";
+
 const Computers = () => {
   return <div>Computers</div>;
 };
