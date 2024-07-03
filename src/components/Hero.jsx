21282b81ad34dde7db83e75e679cf3ede2439d7f
fernandogbz <fgjv06@gmail.com>
2024-07-03 05:30:30 -0400

    Scroll down animated div created with motion

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -27,6 +27,21 @@ const Hero = () => {
           </div>
         </div>
         <ComputersCanvas />
+        <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
+          <a href="#about">
+            <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
+              <motion.dev
+                animate={{ y: [0, 24, 0] }}
+                transition={{
+                  repeat: Infinity,
+                  duration: 1.5,
+                  repeatType: "loop",
+                }}
+                className="w-3 h-3 rounded-full bg-secondary"
+              />
+            </div>
+          </a>
+        </div>
       </section>
     </>
   );
