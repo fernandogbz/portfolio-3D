c594821ec84026cb214f86e4ad265f17d450905e
fernandogbz <fgjv06@gmail.com>
2024-07-19 06:23:52 -0400

    ServiceCard component created, tilt with motion div for expertise or roles added with styles

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,6 +6,22 @@ import { styles } from "../styles";
 import { services } from "../constants";
 import { fadeIn, textVariant } from "../utils/motion";
 
+const ServiceCard = ({ index, title, icon }) => {
+  return (
+    <Tilt className="xs:w-[250px] w-full">
+      <motion.div
+        variants={fadeIn("right", "spring", 0.5 * index, 0.75)}
+        className="w-full green-pink-gradient rounded-[20px] p-[1px] shadow-card"
+      >
+        <div
+          options={{ max: 45, scale: 1, speed: 450 }}
+          className="bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex flex-col justify-evenly items-center"
+        ></div>
+      </motion.div>
+    </Tilt>
+  );
+};
+
 const About = () => {
   return (
     <>
@@ -25,6 +41,12 @@ const About = () => {
         innovadoras que resuelvan problemas reales. Trabajemos juntos para darle
         vida a tus ideas.
       </motion.p>
+
+      <div className="mt-20 flex flex-wrap gap-10">
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index={index} {...service} />
+        ))}
+      </div>
     </>
   );
 };
