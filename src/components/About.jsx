a9ca53bc565ba448d0f94f8a3f762264570f4c42
fernandogbz <fgjv06@gmail.com>
2024-07-19 06:27:10 -0400

    ServiceCard icons added

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -16,7 +16,9 @@ const ServiceCard = ({ index, title, icon }) => {
         <div
           options={{ max: 45, scale: 1, speed: 450 }}
           className="bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex flex-col justify-evenly items-center"
-        ></div>
+        >
+          <img src={icon} alt={title} className="w-16 h-16 object-contain" />
+        </div>
       </motion.div>
     </Tilt>
   );
