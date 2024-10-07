29b5b2fd6c4f806f5a9a06933b5c5e0df49bf320
fernandogbz <fgjv06@gmail.com>
2024-10-07 07:30:47 -0300

    Experience card text changed

--- src/constants/index.js
+++ src/constants/index.js
@@ -116,16 +116,24 @@ const technologies = [
 
 const experiences = [
   {
-    title: "React.js Developer",
-    company_name: "Starbucks",
+    title: "Desarrollador Full Stack Java",
+    company_name: "Coding Dojo",
     icon: starbucks,
     iconBg: "#383E56",
-    date: "March 2020 - April 2021",
+    date: "Enero 2024 - Junio 2024",
     points: [
-      "Developing and maintaining web applications using React.js and other related technologies.",
-      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-      "Implementing responsive design and ensuring cross-browser compatibility.",
-      "Participating in code reviews and providing constructive feedback to other developers.",
+      "Adquirí un sólido dominio del ciclo de vida completo del desarrollo de software, desde el diseño hasta la implementación, mediante el desarrollo de actividades prácticas y la elaboración de proyectos, logrando crear sistemas que son robustos, seguros y eficientes; gracias a un programa de estudio intensivo que considera 320 horas académicas.",
+
+      "Comprendí, diseñé e implementé algoritmos eficientes, así como la aplicación de patrones de diseño y buenas prácticas de programación.",
+
+      "Desarrollé aplicaciones web utilizando Java (Spring), JavaScript y MySQL para almacenar y gestionar información de manera eficiente, lo que permite el desarrollo tanto de back-end como de front-end, así como la familiaridad con diversas herramientas, frameworks y paquetes, los cuales agilizan los procesos y mejoran la calidad del código.",
+
+      "Implementé diseños responsivos y que garantizan la compatibilidad entre navegadores.",
+
+      "Diseñé y normalicé esquemas de bases de datos, estableciendo relaciones entre entidades para garantizar la integridad de los datos, al igual que realizar búsquedas y filtrados eficientes.",
+
+      "Colaboré con equipos multifuncionales, incluidos facilitadores, profesores y otros desarrolladores, fomentando la generación de ideas y la resolución conjunta de problemas.",
+      "Participé en revisiones de código y brindé comentarios constructivos a otros desarrolladores.",
     ],
   },
   {
