4eaf37ed04055f95b19a566daa721f1644cddb26
fernandogbz <fgjv06@gmail.com>
2024-10-11 07:53:21 -0300

    projects detail paragraph with motion added

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -14,6 +14,19 @@ const Works = () => {
         <p className={styles.sectionSubText}>Mi trabajo</p>
         <h2 className={styles.sectionHeadText}>Proyectos</h2>
       </motion.div>
+
+      <div className="w-full flex">
+        <motion.p
+          variants={fadeIn("", "", 0.1, 1)}
+          className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]"
+        >
+          Los siguientes proyectos muestran mis habilidades y experiencia a
+          través de ejemplos reales de mi trabajo. Cada proyecto se describe
+          brevemente con enlaces a repositorios de código y demostraciones en
+          vivo. Refleja mi capacidad para resolver problemas complejos, trabajar
+          con diferentes tecnologías y gestionar proyectos de manera eficaz.
+        </motion.p>
+      </div>
     </>
   );
 };
