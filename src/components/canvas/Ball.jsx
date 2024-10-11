cfc0b205dd9b24236ea5a342b23dfa47d4a1f11b
fernandogbz <fgjv06@gmail.com>
2024-10-11 07:27:42 -0300

    mesh added, inside it ball shape, material and decal (the image of the technology)

--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -8,15 +8,30 @@ import {
   useTexture,
 } from "@react-three/drei";
 
-import { CanvasLoader } from "../Loader";
+import CanvasLoader from "../Loader";
 
 const Ball = (props) => {
   const [decal] = useTexture([props.imgUrl]);
 
   return (
     <Float speed={1.7} rotationIntensity={1} floatIntensity={2}>
-      <ambientLight intensity={0.5} />
-      <directionalLight position={[0, 0, 0.05]} />
+      <ambientLight intensity={0.6} />
+      <directionalLight position={[0, 0, 0.65]} />
+      <mesh castShadow receiveShadow scale={2.75}>
+        <icosahedronGeometry args={[1, 1]} />
+        <meshStandardMaterial
+          color="#a1cdfa7c"
+          polygonOffset
+          polygonOffsetFactor={-5}
+          flatShading
+        />
+        <Decal
+          map={decal}
+          position={[0, 0, 1]}
+          rotation={[0, 0, 0]}
+          flatShading
+        />
+      </mesh>
     </Float>
   );
 };
