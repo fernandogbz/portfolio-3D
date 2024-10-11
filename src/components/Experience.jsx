1d5af37c05b7a34d88f576523c3234bfda8e5b52
fernandogbz <fgjv06@gmail.com>
2024-10-11 06:31:35 -0300

    experienceCard contentArrowStyle color changed

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -18,7 +18,7 @@ import { textVariant } from "../utils/motion";
 const ExperienceCard = ({ experience }) => (
   <VerticalTimelineElement
     contentStyle={{ background: "#1d1836", color: "#fff" }}
-    contentArrowStyle={{ borderRight: "7px solid  #232631" }}
+    contentArrowStyle={{ borderRight: "7px solid  #1d1836" }}
     date={experience.date}
     iconStyle={{ background: experience.iconBg }}
     icon={
