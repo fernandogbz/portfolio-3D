96e2817c4a50f6f4ab38957e4878713ed79e8ee8
fernandogbz <fgjv06@gmail.com>
2024-10-11 19:50:23 -0300

    serviceCard centered

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -48,7 +48,7 @@ const About = () => {
         vida a tus ideas.
       </motion.p>
 
-      <div className="mt-20 ml-8 flex flex-wrap gap-10">
+      <div className="mt-20 flex flex-wrap gap-10 justify-center">
         {services.map((service, index) => (
           <ServiceCard key={service.title} index={index} {...service} />
         ))}
