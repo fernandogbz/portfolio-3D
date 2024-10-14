0eb981f9dead3984ee32966e5b85a41db80a1de7
fernandogbz <fgjv06@gmail.com>
2024-10-14 17:10:35 -0300

    mapping through each testimonial to show feedbackCard

--- src/components/Feedbacks.jsx
+++ src/components/Feedbacks.jsx
@@ -16,6 +16,11 @@ const Feedbacks = () => {
           <h2 className={styles.sectionHeadText}>Testimonios</h2>
         </motion.div>
       </div>
+      <div className={`${styles.paddingX} -mt-20 pb-14 flex flex-wrap gap-7`}>
+        {testimonials.map((testimonial, index) => (
+          <FeedbackCard key={testimonial.name} index={index} {...testimonial} />
+        ))}
+      </div>
     </div>
   );
 };
