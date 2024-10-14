22da90f67eea7670084108485f2b04ce50496936
fernandogbz <fgjv06@gmail.com>
2024-10-14 17:27:56 -0300

    testimonials added

--- src/components/Feedbacks.jsx
+++ src/components/Feedbacks.jsx
@@ -16,7 +16,23 @@ const FeedbackCard = ({
   <motion.div
     variants={fadeIn("", "spring", index * 0.5, 0.75)}
     className="bg-black-200 p-10 rounded-3xl xs:w-[320px] w-full"
-  ></motion.div>
+  >
+    <div className="mt-1">
+      <p>"{testimonial}"</p>
+
+      <div className="mt-7 flex justify-between items-center gap-1">
+        <div className="flex-1 flex flex-col">
+          <p>
+            <span>@</span>
+            {name}
+          </p>
+          <p>
+            {designation} of {company}
+          </p>
+        </div>
+      </div>
+    </div>
+  </motion.div>
 );
 
 const Feedbacks = () => {
