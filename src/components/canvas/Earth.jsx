c6da05268e74e97dc94b8acd040f2782260a637a
fernandogbz <fgjv06@gmail.com>
2024-10-15 18:11:45 -0300

    EarthCanvas component added, renders the Canvas with specifications about orbitControls and Earth component

--- src/components/canvas/Earth.jsx
+++ src/components/canvas/Earth.jsx
@@ -7,4 +7,27 @@ const Earth = () => {
   return <div>Earth</div>;
 };
 
-export default Earth;
+const EarthCanvas = () => {
+  return (
+    <Canvas
+      shadows
+      frameloop="demand"
+      gl={{ preserveDrawingBuffer: true }}
+      // camera={{ position: [0, 0, 10], fov: 60 }}
+      // style={{ height: "100vh", width: "100%" }}
+    >
+      <Suspense fallback={<CanvasLoader />}>
+        <OrbitControls
+          autoRotate
+          enableZoom={false}
+          maxPolarAngle={Math.PI / 2}
+          minPolarAngle={Math.PI / 2}
+        />
+        <Earth />
+        {/* <Preload all /> */}
+      </Suspense>
+    </Canvas>
+  );
+};
+
+export default EarthCanvas;
