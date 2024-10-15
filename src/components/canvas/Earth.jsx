6cc42038f6cef749e30da567ff2608a591089118
fernandogbz <fgjv06@gmail.com>
2024-10-15 18:31:13 -0300

    Earth component renders earth 3d model

--- src/components/canvas/Earth.jsx
+++ src/components/canvas/Earth.jsx
@@ -1,10 +1,14 @@
 import { Suspense } from "react";
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
-import { CanvasLoader } from "../Loader";
+import CanvasLoader from "../Loader";
 
 const Earth = () => {
-  return <div>Earth</div>;
+  const earth = useGLTF("./planet/scene.gltf");
+
+  return (
+    <primitive object={earth.scene} scale={2.5} position-y={0} rotation-y={0} />
+  );
 };
 
 const EarthCanvas = () => {
@@ -13,8 +17,7 @@ const EarthCanvas = () => {
       shadows
       frameloop="demand"
       gl={{ preserveDrawingBuffer: true }}
-      // camera={{ position: [0, 0, 10], fov: 60 }}
-      // style={{ height: "100vh", width: "100%" }}
+      camera={{ fov: 45, near: 0.1, far: 200, position: [-4, 3, 6] }}
     >
       <Suspense fallback={<CanvasLoader />}>
         <OrbitControls
