17cba55ed06baf687e3c4e16e0e0a333093fd4cb
fernandogbz <fgjv06@gmail.com>
2024-11-01 15:58:49 -0300

    profile pic left

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -11,8 +11,11 @@ const Hero = () => {
     <>
       <section className="relative w-full h-screen mx-auto">
         <div
-          className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}
+          className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row align-middle items-start gap-5`}
         >
+          <picture className="w-[20%] overflow-hidden object-contain rounded-full border-4 border-[#915eff] sm:block hidden">
+            <img src={fotofer} alt="foto-fernando" />
+          </picture>
           <div className="flex flex-col justify-center items-center mt-5">
             <div className="w-5 h-5 rounded-full bg-[#915eff]" />
             <div className="w-1 sm:h-80 h-40 violet-gradient" />
@@ -28,9 +31,6 @@ const Hero = () => {
                 <br className="sm:block hidden" /> y aplicaciones web
               </p>
             </div>
-            <picture className="w-[20%] overflow-hidden object-contain rounded-full border-4 border-[#915eff] sm:block hidden">
-              <img src={fotofer} alt="foto-fernando" />
-            </picture>
           </div>
         </div>
         <ComputersCanvas />
