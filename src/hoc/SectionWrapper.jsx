fd149d271b7ccdc94f0c6cddd770f1a7d324e3c0
Fernando <fgjv06@gmail.com>
2026-04-16 12:37:17 -0400

    Refactor SectionWrapper to accept custom className for improved styling flexibility and visibility on scroll

--- src/hoc/SectionWrapper.jsx
+++ src/hoc/SectionWrapper.jsx
@@ -3,7 +3,7 @@ import { motion } from "framer-motion";
 import { styles } from "../styles";
 import { staggerContainer } from "../utils/motion";
 
-const SectionWrapper = (Component, idName) =>
+const SectionWrapper = (Component, idName, className = styles.padding) =>
   function HOC() {
     return (
       <motion.section
@@ -11,7 +11,7 @@ const SectionWrapper = (Component, idName) =>
         initial="hidden"
         whileInView="show"
         viewport={{ once: true, amount: 0.1 }}
-        className={`${styles.padding} max-w-7xl mx-auto relative z-0`}
+        className={`${className} max-w-7xl mx-auto relative z-0`}
       >
         <span className="hash-span" id={idName}>
           &nbsp;
